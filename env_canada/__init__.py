e585ee5eb9bfa3be710fac7dfdf23bcc06a5be7d
Brian Fitzgerald <fitzb@users.noreply.github.com>
2026-02-09 00:15:46 -0400

    Replace voluptuous with pydantic. Add toml formatter. Move pyupgrade to ruff.

--- env_canada/__init__.py
+++ env_canada/__init__.py
@@ -14,6 +14,6 @@ from .ec_aqhi import ECAirQuality
 from .ec_alerts import ECAlerts
 from .ec_historical import ECHistorical, ECHistoricalRange
 from .ec_hydro import ECHydro
-from .ec_radar import ECRadar
 from .ec_map import ECMap
+from .ec_radar import ECRadar
 from .ec_weather import ECWeather, ECWeatherUpdateFailed
