e7dd8bd7088722bb603f6c480c9c812b2e2cb78a
Brian Fitzgerald <28903514+fitzb@users.noreply.github.com>
2026-03-07 00:37:23 -0400

    Apply linters.

--- env_canada/__init__.py
+++ env_canada/__init__.py
@@ -10,8 +10,8 @@ __all__ = [
     "ECWeatherUpdateFailed",
 ]
 
-from .ec_aqhi import ECAirQuality
 from .ec_alerts import ECAlerts
+from .ec_aqhi import ECAirQuality
 from .ec_historical import ECHistorical, ECHistoricalRange
 from .ec_hydro import ECHydro
 from .ec_map import ECMap
